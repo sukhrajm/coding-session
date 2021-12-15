50f34ddf3301c2dd839bd5144a37356ed82d0902
Sukhraj Rattan <sukhraj.rattan@tesco.com>
2021-12-15 15:14:00 +0000

    first commit

---
+++ src/test/java/task/MyExampleTest.java
@@ -0,0 +1,14 @@
+package task;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyExampleTest {
+
+    @Test
+    public void testSomething() {
+        String a = "hello";
+        assertEquals("hello", a);
+    }
+}
